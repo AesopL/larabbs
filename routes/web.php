06cb736495bb116b785ee7660d3e121d1cc32060
AesopL <wenduan@outlook.com>
2018-04-11 06:06:13 +0000

    User profile page

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,12 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', 'PagesController@index');
 
-
 Auth::routes();
 
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+//Route::get('/users/{user}','UsersController@show')->name('users.show');
