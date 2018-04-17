e9163afc3513f27bc0f108886cfb3c2ece0cd8de
AesopL <wenduan@outlook.com>
2018-04-17 06:08:52 +0000

    Topic order

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -6,13 +6,42 @@ class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'catergory_id', 'reply_count', 'view_count', 'last_reply_user_id', 'excerpt', 'slug'];
 
+    //一篇文章属于一个分类
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
+
+    //一篇文章属于一个用户
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function scopeWithOrder($query, $order)
+    {
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+
+            default:
+                $query->recentReplied();
+                break;
+        }
+        //预加载，防止N+1问题
+        return $query->with('user', 'category');
+    }
+
+    public function scopeRecentReplied($query)
+    {
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    //按照创建时间排序
+    public function scopeRecent($query)
+    {
+        return $query->orderBy('created_at', 'desc');
     }
 
 }
