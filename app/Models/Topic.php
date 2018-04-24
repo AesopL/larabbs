feed0814adde0196e931c72d3eb103f712ac2146
AesopL <wenduan@outlook.com>
2018-04-24 01:02:24 +0000

    Auto translate slug

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -44,4 +44,10 @@ class Topic extends Model
         return $query->orderBy('created_at', 'desc');
     }
 
+    //slug链接
+    public function link($params = [])
+    {
+        return route('topics.show', array_merge([$this->id, $this->slug], $params));
+    }
+
 }
